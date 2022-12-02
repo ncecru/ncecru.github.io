4449ef344cf00d9ccaa9594fad531d47d020a6e8
ncecru <119504163+ncecru@users.noreply.github.com>
2022-12-02 09:54:09 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,9 +14,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "d047bb423f8e3d569ad313a1e3c4753d",
-"/": "d047bb423f8e3d569ad313a1e3c4753d",
-"main.dart.js": "85678097b312c919c79ed31baa25a394",
+"index.html": "9deff99ae9272ba31abcd1c3d6d4c720",
+"/": "9deff99ae9272ba31abcd1c3d6d4c720",
+"main.dart.js": "3fdd77eb0c8f732df04e42c281b4027f",
 "manifest.json": "fcfaa5b56500b68ac097a57f30734e40",
 "version.json": "67435d60a0fb297c3069ca359e216ccb"
 };
